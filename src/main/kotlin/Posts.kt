806b1572ae53325c4f8468875c2a4604d41abfff
Odysseus <brilliantbatman81@gmail.com>
2025-06-09 20:49:23 +0500

    Initial commit

---
+++ src/main/kotlin/Posts.kt
@@ -0,0 +1,63 @@
+data class Post(
+    var id: Int,
+    val authorId: Int,
+    val fromId: Int = 0,
+    val createdBy: Int = 0,
+    val replyOwnerId: Int = 0,
+    val replyPostId: Int = 0,
+    val friendsOnly: Boolean = false,
+    val date: Int = 0,
+    val isFavourite: Boolean = false,
+    val authorName: String,
+    val content: String,
+    val published: Long,
+    val likes: Int,
+    val signerId: Int = 0,
+    val canPin: Boolean = false
+) {
+
+    class Comments(count: Int, canPost: Boolean, groupsCanPost: Boolean, canClose: Boolean, canOpen: Boolean)
+
+    class Copyright(id: Int, link: String, name: String, type: String)
+
+}
+
+class WallService {
+
+    private var posts = emptyArray<Post>()
+    private var uniqueId = 0
+
+    fun add(post: Post): Post {
+        uniqueId += 1
+        val newPost = post.copy(id = uniqueId + 1)
+        posts += newPost
+        return posts.last()
+    }
+
+    fun update(post: Post): Boolean {
+
+        for ((index, currentPost) in posts.withIndex()) {
+            if (post.id == currentPost.id) {
+                posts[index] = post
+                return true
+            }
+        }
+        return false
+    }
+
+    fun likeById(id: Int) {
+        for ((index, post) in posts.withIndex()) {
+            if (post.id == id) {
+                posts[index] = post.copy(likes = post.likes + 1)
+            }
+        }
+    }
+}
+
+fun main() {
+    val post = Post(1, 1, authorName = "author", content = "content", likes = 0, published = 0)
+    val liked = post.copy(likes = post.likes + 1)
+    val (id, authorId, _, _, _, _, _, _, _, _, content) = post
+    println(liked)
+    println("$id, $authorId, $content")
+}
---
+++ src/test/kotlin/WallServiceTest.kt
@@ -0,0 +1,58 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class WallServiceTest {
+
+    @Test
+    fun add() {
+        val service = WallService()
+        val newPost = Post(
+            id = 1,
+            authorId = 165,
+            authorName = "Some Name",
+            content = "Some content",
+            published = 121111,
+            likes = 1,
+        )
+
+        val addedPost = service.add(newPost)
+
+        assertNotEquals(0, addedPost.id)
+    }
+
+    @Test
+    fun updatePostWithExistingID() {
+        val service = WallService()
+        val post = service.add(
+            Post(
+                id = 0,
+                authorId = 1,
+                authorName = "Name",
+                content = "content",
+                published = 7,
+                likes = 10,
+            )
+        )
+
+        val updatedPost = post.copy(content = "New content added", likes = 115)
+
+        assertTrue(service.update(updatedPost))
+    }
+
+    @Test
+    fun updateNonExistingPost() {
+        val service = WallService()
+        val post = service.add(
+            Post(
+                id = 1_000_000_000,
+                authorId = 1,
+                authorName = "Name",
+                content = "content",
+                published = 10,
+                likes = 7,
+            )
+        )
+        assertTrue(service.update(post))
+    }
+}
